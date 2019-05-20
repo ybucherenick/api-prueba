e3f3e0ceccddc646047eac8c84575e66c4c95bf2
Yolymar Bucherenick <tomas.diaz06.p@gmail.com>
2019-05-20 13:14:07 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.send('Funciona');
+})
+
+app.listen(3000);
+console.log('Server Started on Port 3000');
+
+module.exports = app;
